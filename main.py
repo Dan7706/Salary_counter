bea0b3e8c98ceb0a614beefe3f6a4fb179280ce0
Dan7706 <vardanyengibaryan@gmail.com>
2024-05-22 14:20:30 +0800

    Echo bot is ready '

---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+from aiogram import types, Bot, Dispatcher
+from aiogram.filters import CommandStart, Command
+
+
+TOKEN = "7199238139:AAEZQmHH5GZ2BTi3kFH1bzkmbHODp4dtbpE"
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer(f"Hi {message.from_user.full_name}")
+
+
+
+
+@dp.message()
+async def echo(message: types.Message):
+    print(message.text)
+    await bot.send_message(message.from_user.id, message.text)
+
+
+
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+
+
+asyncio.run(main())
+
